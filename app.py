01626746728ed035a5a9256dc8c8917709045976
gabee1987 <gabee1987@gmail.com>
2017-10-19 15:41:17 +0200

    added homeworld name and character descreption from db

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ def characters_page():
 
 @app.route('/characters/<character_id>')
 def detailed_characters(character_id):
-    character_data = request.get('https://swapi.co/api/people/%d' % character_id).json()
+    character_data = requests.get('https://swapi.co/api/people/%s' % character_id).json()
     character_description = bll.get_character_description(character_id)
-    return render_template('detailed-character.html', character_data=character_data)
+    return render_template('detailed_character.html', character_data=character_data, character_description=character_description, character_id=character_id)
 
 
 @app.route('/login', methods=['GET'])
--- static/js/detailed_character.js
+++ static/js/detailed_character.js
@@ -1,13 +1,31 @@
 $(document).ready(main);
 
 function main() {
+    /*var allUrls = getUrls();
+    console.log(allUrls);
+    for (let i = 0; i < allUrls.length; i++) {
+        getCharacterHomeworld(i);
+    }*/
     getCharacterHomeworld();
+    //getSPeciesName();
+
+}
+
+function getUrls() {
+    var homeWorldUrl = $('#homeworldUrl').html();
+    var speciesUrl = $('#speciesUrl').html();
+    var vehiclesUrl = $('#vehiclesUrl').html();
+    var starshipsUrl = $('#starshipsUrl').html();
+    var filmsUrl = $('#filmsUrl').html();
+
+    var allUrls = [homeWorldUrl, speciesUrl, vehiclesUrl, starshipsUrl, filmsUrl];
+    return allUrls;
 }
 
 function getCharacterHomeworld() {
     //var charID = window.location.pathname.split('/').pop();
-    var homeWorldUrl = $('#homeworld').html();
-    console.log(homeWorldUrl);
+    var homeWorldUrl = $('#homeworldUrl').html();
+    //console.log(homeWorldUrl);
     $.ajax({
             type: 'GET',
             url: homeWorldUrl,
@@ -21,12 +39,65 @@ function getCharacterHomeworld() {
         });
 }
 
-function replaceHomeworldName(response) {
-    console.log(response);
-    var homeWorldData = response['results'];
-    console.log(homeWorldData);
-    var homeWorldName = $('#homeworld');
-    homeWorldName.html('');
-    homeWorldName.append(homeWorldData[name]);
-    console.log(homeWorldData[name]);
+/*
+function getSPeciesName() {
+    //var charID = window.location.pathname.split('/').pop();
+    var speciesUrl = $('#speciesUrl').html();
+    //console.log(homeWorldUrl);
+    $.ajax({
+            type: 'GET',
+            url: speciesUrl,
+            dataType: 'json',
+            success: function(response) {
+                replaceSPeciesNames(response);
+            },
+            error: function() {
+                alert('Error in network request!');
+            } 
+        });
 }
+*/
+
+function replaceHomeworldName(response) {
+    var homeWorldData = response;
+    var homeWorldName = $('#homeworldUrl');
+    homeWorldName.html('');
+    homeWorldName.append(homeWorldData['name']);
+}
+
+/*
+function replaceSPeciesNames(response) {
+    var speciesData = response['results'];
+    var speciesName = $('#speciesUrl');
+    speciesName.html('');
+    speciesName.append(speciesData['name']);
+}
+*/
+
+/*function replacecharacterBioUrlName(response) {
+    var homeWorldData = response;
+    var homeWorldName = $('#homeworldUrl');
+    homeWorldName.html('');
+    homeWorldName.append(homeWorldData['name']);
+
+    var speciesData = response['results'];
+    var speciesName = $('#speciesUrl');
+    speciesName.html('');
+    speciesName.append(speciesData['name']);
+
+    var vehiclesData = response['results'];
+    var vehiclesName = $('#vehiclesUrl');
+    vehiclesName.html('');
+    vehiclesName.append(vehiclesData['name']);
+
+    var starshipsData = response['results'];
+    var starshipsName = $('#starshipsUrl');
+    starshipsName.html('');
+    starshipsName.append(starshipsData['name']);
+
+    var filmsData = response['results'];
+    var filmsName = $('#filmsUrl');
+    filmsName.html('');
+    filmsName.append(filmsData['name']);
+
+}*/
